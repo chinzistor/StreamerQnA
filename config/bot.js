f81d12f95af5fef2751bbab5c174be6c64a40ed1
chinzistor <illmate0226@gmail.com>
2024-07-04 18:49:18 +0200

    Main readme file

--- config/bot.js
+++ config/bot.js
@@ -1,3 +1,3 @@
-window.botUsername = "";
-window.oauthToken = "";
-window.channel = ""
+window.botUsername = "Chinzisbot";
+window.oauthToken = "oauth:n3a6d7oqaf7eu1aplnh4dirwnlmujy";
+window.channel = "#chinzistor"
