e3ba0cc6b6c67f1de7e3b3a128a39bb14caa87d5
chinzistor <illmate0226@gmail.com>
2024-07-04 18:41:05 +0200

    Docker source style config

---
+++ config/dashboard.js
@@ -0,0 +1,90 @@
+window.dashboardStyle = {
+    "background": "#000033",
+    "rounding": 10,
+    "buttons": {
+        "selected": {
+            "background": "#00AAAA",
+            "hoverColor": "#006666",
+            "hoverTime": "1.0s",
+            "text": "#FFFF00",
+            "italic": false,
+            "bold": true,
+            "font": "sans serif",
+            "size": 14,
+            "border": {
+                "thickness": "2px",
+                "style": "solid",
+                "color": "#FFFF00"
+            }
+        },
+        "unselected": {
+            "background": "#0000AA",
+            "hoverColor": "#0000FF",
+            "hoverTime": "0.5s",
+            "text": "#00FFFF",
+            "italic": true,
+            "bold": false,
+            "font": "sans serif",
+            "size": 12,
+            "border": {
+                "thickness": "2px",
+                "style": "solid",
+                "color": "#00FFFF"
+            }
+        }
+    },
+    "queue": {
+        "selected": {
+            "name": {
+                "background": "#33FFFF",
+                "color": "#AAAA00",
+                "italic": false,
+                "bold": true,
+                "size": 15,
+                "font": "sans serif"
+            },
+            "time": {
+                "background": "#33FFFF",
+                "color": "#888800",
+                "italic": true,
+                "bold": false,
+                "size": 15,
+                "font": "sans serif"
+            },
+            "question": {
+                "background": "#008888",
+                "color": "#AAAA00",
+                "italic": false,
+                "bold": false,
+                "size": 15,
+                "font": "sans serif"
+            }
+        },
+        "unselected": {
+            "name": {
+                "background": "#0000AA",
+                "color": "#00FFFF",
+                "italic": false,
+                "bold": true,
+                "size": 15,
+                "font": "sans serif"
+            },
+            "time": {
+                "background": "#0000AA",
+                "color": "#00AAAA",
+                "italic": true,
+                "bold": false,
+                "size": 15,
+                "font": "sans serif"
+            },
+            "question": {
+                "background": "#000055",
+                "color": "#00FFFF",
+                "italic": false,
+                "bold": false,
+                "size": 15,
+                "font": "sans serif"
+            }
+        }
+    }
+};
