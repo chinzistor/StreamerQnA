0b01d144792027b003c041c8cfe43c2057b4dc96
chinzistor <illmate0226@gmail.com>
2024-07-04 18:38:13 +0200

    Commands config

---
+++ config/commands.js
@@ -0,0 +1,19 @@
+window.commands = {
+    "questions": {
+        "response": "your question has been saved.",
+        "aliases": [
+            "question"
+        ]},
+    "requests": {
+        "response": "your request has been stored.",
+        "aliases": [
+            "request",
+            "req"
+        ]},
+    "compliments": {
+        "response": "we've recieved your compliment.",
+        "aliases": [
+            "compliment",
+            "comp"
+        ]}
+};
