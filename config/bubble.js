3e2ac6133a5bc213858ee0ecdea5cd486e6368dd
chinzistor <illmate0226@gmail.com>
2024-07-04 18:37:57 +0200

    Browser source design config

---
+++ config/bubble.js
@@ -0,0 +1,58 @@
+window.bubbleStyle = {
+    "texts": {
+        "name": {
+            "background": "#33FFFF",
+            "color": "#AAAA00",
+            "italic": false,
+            "bold": true,
+            "size": 15,
+            "font": "sans serif"
+        },
+        "time": {
+            "background": "#33FFFF",
+            "color": "#888800",
+            "italic": true,
+            "bold": false,
+            "size": 15,
+            "font": "sans serif",
+            "format": "%h:%m:%s %p"
+        },
+        "question": {
+            "background": "#008888",
+            "color": "#AAAA00",
+            "italic": false,
+            "bold": false,
+            "size": 15,
+            "font": "sans serif"
+        }
+    },
+    "style": {
+        "rounding": 20,
+        "body": {
+            "transparency": 1,
+            "color": "#AAFFFF"
+        },
+        "header": {
+            "transparency": 1,
+            "color": "#00FFFF"
+        }
+    },
+    "profile": {
+        "show": true,
+        "size": 100
+    },
+    "animation": {
+        "in": {
+            "style": "none",
+            "origin": "center",
+            "center": "center",
+            "direction": "left"
+        },
+        "out": {
+            "style": "none",
+            "origin": "center",
+            "center": "center",
+            "direction": "left"
+        }
+    }
+};
