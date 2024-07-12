ad8f529226dbbf95419d06a289a2575394c78327
chinzistor <illmate0226@gmail.com>
2024-07-12 23:56:35 +0200

    Script to read and manage stored data

--- scripts/dashboard/readData.js
+++ scripts/dashboard/readData.js
@@ -1,16 +1,32 @@
+// Reading stored questions and adding them to the dashboard
 function readData() {
     window.id = 0;
+    // Reseting the bubble
     localStorage.setItem('bubbleData', '-none-');
+    // Listing all keys
+    let keys = [];
     for (let a = localStorage.length; a > 0; a--) {
         const key = localStorage.key(a - 1);
+        keys.push(key);
+    }
+    // Sorting the questions alphabetically
+    keys.sort()
+    // Going through all questions
+    for (let a = 0; a < keys.length; a++) {
         for (let group in window.commands) {
+        const key = keys[a];
+            // Checking if the key of a data contains one of the groups' name
             if (key.includes(group)) {
-                const id = +key.replace(group, '');
+                // Getting the numerical ID of the group
+                const id = key.replace(group, '');
+                const numId = +id;
                 //console.log(window.id);
-                if (id >= window.id) {
-                    window.id = id + 1;
+                // Making sure upcoming questions from chat will have a unique numerical ID
+                if (numId >= window.id) {
+                    window.id = numId + 1;
                 }
                 //console.log(window.id);
+                // Splitting up the data into usable variables
                 const value = localStorage.getItem(key);
                 let index1 = value.indexOf('#');
                 let user = value.slice(0, index1);
@@ -19,7 +35,8 @@ function readData() {
                 let question = value.slice(index2 + "#".length);
                 let questionGroup = key.replace(id, '');
 
-                addQuestion(user, questionGroup, time, question, false, id, '')
+                // Handling the data
+                addQuestion(user, questionGroup, time, question, false, id, "")
                 //console.log(`${user} ${time} ${question} ${questionGroup} ${id} ${key}`);
             }
         }
