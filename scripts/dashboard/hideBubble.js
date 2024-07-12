ac93f72d3bca1c61507dcceae000fb9719982170
chinzistor <illmate0226@gmail.com>
2024-07-12 23:56:11 +0200

    Script to hide the bubble

--- scripts/dashboard/hideBubble.js
+++ scripts/dashboard/hideBubble.js
@@ -1,7 +1,10 @@
+// Hiding bubble
 function hideBubble(id, group) {
     const textIds = ['name', 'time', 'question'];
     const tableIds = ['nameCell', 'timeCell', 'questionCell'];
+    // Making sure all parts of the queue item are reset
     for (let a = 0; a < textIds.length; a++) {
+        // Styling text
         let textElement = document.getElementById(`${textIds[a]}${group}${id}`);
         if (textElement) {
             textElement.style.fontFamily = window.dashboardStyle.queue.unselected[textIds[a]].font;
@@ -12,16 +15,20 @@ function hideBubble(id, group) {
         }
     }
     for (let a = 0; a < tableIds.length; a++) {
+        // Styling box
         let tableElement = document.getElementById(`${tableIds[a]}${group}${id}`);
         if (tableElement) {
             tableElement.style.backgroundColor = window.dashboardStyle.queue.unselected[textIds[a]].background;
         }
     }
+    // Setting show/hide image to show
     document.getElementById(`show${group}${id}`).src = 'assets/show.png';
     document.getElementById(`show${group}${id}`).alt = 'Show this message on stream.';
+    // Setting show/hide onclick function to show
     document.getElementById(`show${group}${id}`).onclick = function () {
         showBubble(id, group);
     }
+    // Setting the showed item in localStorage to none, so the bubble hides itself
     localStorage.setItem('bubbleData', '-none-');
     //console.log(localStorage.getItem('bubbleData'));
 }
