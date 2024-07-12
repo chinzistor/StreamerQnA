2d56957194d92da9ed356b06145ec1317428037e
chinzistor <illmate0226@gmail.com>
2024-07-12 23:56:44 +0200

    Script to show the bubble

--- scripts/dashboard/showBubble.js
+++ scripts/dashboard/showBubble.js
@@ -1,17 +1,23 @@
+// Showing question in bubble
 function showBubble(id, groupOriginal) {
+    // Resetting all queues' items to the hidden state
     const textIds = ['name', 'time', 'question'];
     const tableIds = ['nameCell', 'timeCell', 'questionCell'];
+    // Making sure all parts of the queue item are reset
     for (let group in window.commands) {
         for (let index = 0; index < window.id; index++) {
             const element = document.getElementById(`show${group}${index}`)
             if (element) {
+                // Setting show/hide image to show
                 element.src = 'assets/show.png';
                 element.alt = 'Show this message on stream.';
+                // Setting show/hide onclick function to show
                 element.onclick = function () {
                     showBubble(index, group);
                 }
             }
             for (let a = 0; a < textIds.length; a++) {
+                // Styling text
                 let textElement = document.getElementById(`${textIds[a]}${group}${index}`);
                 if (textElement) {
                     textElement.style.fontFamily = window.dashboardStyle.queue.unselected[textIds[a]].font;
@@ -22,6 +28,7 @@ function showBubble(id, groupOriginal) {
                     }
             }
             for (let a = 0; a < tableIds.length; a++) {
+                // Styling box
                 let tableElement = document.getElementById(`${tableIds[a]}${group}${index}`)
                 if (tableElement) {
                     tableElement.style.backgroundColor = window.dashboardStyle.queue.unselected[textIds[a]].background;
@@ -29,7 +36,9 @@ function showBubble(id, groupOriginal) {
             }
         }
     }
+    // Setting the selecting item to be shown
     for (let a = 0; a < textIds.length; a++) {
+        // Styling text
         let textElement = document.getElementById(`${textIds[a]}${groupOriginal}${id}`);
         if (textElement) {
             textElement.style.fontFamily = window.dashboardStyle.queue.selected[textIds[a]].font;
@@ -40,16 +49,20 @@ function showBubble(id, groupOriginal) {
         }
     }
     for (let a = 0; a < tableIds.length; a++) {
+        // Styling box
         let tableElement = document.getElementById(`${tableIds[a]}${groupOriginal}${id}`);
         if (tableElement) {
             tableElement.style.backgroundColor = window.dashboardStyle.queue.selected[textIds[a]].background;
         }
     }
+    // Setting show/hide image to hide
     document.getElementById(`show${groupOriginal}${id}`).src = 'assets/hide.png';
     document.getElementById(`show${groupOriginal}${id}`).alt = 'Hide this message on stream.';
+    // Setting show/hide onclick function to hide
     document.getElementById(`show${groupOriginal}${id}`).onclick = function () {
         hideBubble(id, groupOriginal);
     }
+    // Showing the question in the bubble
     localStorage.setItem('bubbleData', `${groupOriginal}${id}`);
     //console.log(localStorage.getItem('bubbleData'));
 }
