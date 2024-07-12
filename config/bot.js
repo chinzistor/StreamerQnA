1ae48059a7e2548e66d8cac3248e436e7c1110ff
chinzistor <illmate0226@gmail.com>
2024-07-12 23:51:15 +0200

    Bot configuration

--- config/bot.js
+++ config/bot.js
@@ -1,3 +1,12 @@
+// Twitch connection config
 window.botUsername = "";
 window.oauthToken = "";
 window.channel = ""
+
+// Duplicated message detection config
+window.allowSQFDU = false; // same question from different users
+window.allowRQ = false; // repeated question
+window.allowSQ = false; // similar questions
+window.checkGroup = true;
+window.duplicationResponse = "this question was already submitted.";
+window.similaritySensitivity = 0.5;
