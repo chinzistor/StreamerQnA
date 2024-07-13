dffe234c8231a7dd32e663d55b7c535258d14753
chinzistor <illmate0226@gmail.com>
2024-07-13 17:15:09 +0200

    Commands configuration

--- config/commands.js
+++ config/commands.js
@@ -3,17 +3,23 @@ window.commands = {
         "response": "your question has been saved.",
         "aliases": [
             "question"
-        ]},
+        ],
+        "duplicationResponse": "this question was already submitted."
+    },
     "requests": {
         "response": "your request has been stored.",
         "aliases": [
             "request",
             "req"
-        ]},
+        ],
+        "duplicationResponse": "this request is already submitted."
+    },
     "compliments": {
         "response": "we've recieved your compliment.",
         "aliases": [
             "compliment",
             "comp"
-        ]}
-}; 
+        ],
+        "duplicationResponse": "yes, we know, someone already said that."
+    }
+};
