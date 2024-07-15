07ee03326e0d3811b7989655db59837d1919c20c
chinzistor <illmate0226@gmail.com>
2024-07-15 15:52:18 +0200

    Script to format time

---
+++ scripts/getTime.js
@@ -0,0 +1,18 @@
+// Getting the local time formatted based on formatting settings
+function getTime(time) {
+    //let now = new Date();
+    //console.log(now);
+    const date = new Date(time);
+    const years = date.getFullYear().toString().padStart(4, '0');
+    const months = (date.getMonth() + 1).toString().padStart(2, '0');
+    const days = date.getDate().toString().padStart(2, '0');
+    const hours24 = date.getHours().toString().padStart(2, '0');
+    const hours12 = hours24 % 12 || 12;
+    const period = hours24 >= 12 ? 'PM' : 'AM';
+    const minutes = date.getMinutes().toString().padStart(2, '0');
+    const seconds = date.getSeconds().toString().padStart(2, '0');
+
+    let format = window.bubbleStyle.texts.time.format;
+    format = format.replace('%y', years).replace('%M', months).replace('%d', days).replace('%h', hours24).replace('%H', hours12).replace('%m', minutes).replace('%s', seconds).replace('%p', period);
+    return format;
+}
