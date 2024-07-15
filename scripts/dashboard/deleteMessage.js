3893e959709e8a142a0610bee2c120a94df8dc59
chinzistor <illmate0226@gmail.com>
2024-07-15 15:51:25 +0200

    Script to remove questions from the dashboard

--- scripts/dashboard/deleteMessage.js
+++ scripts/dashboard/deleteMessage.js
@@ -3,6 +3,6 @@ function deleteMessage(id, group) {
     hideBubble(id, group);
     localStorage.removeItem(`${group}${id}`);
     localStorage.setItem('bubbleData', '-none-');
-    //console.log(localStorage.getItem('bubbleData'));
+    //console.log(`${group}${id}`);
     document.getElementById(`${group}${id}`).remove();
 }
