984fddafe02b6bb67f76146921ebea88834c54db
chinzistor <illmate0226@gmail.com>
2024-09-14 15:04:00 +0200

    Script to check latest version

--- scripts/checkUpdate.js
+++ scripts/checkUpdate.js
@@ -1,8 +1,7 @@
 async function checkUpdate() {
     // Version of the system is saved here
     // I know it's dumb placement, but it was the simplest
-    const version = '0.41.8';
-    let latestVersion;
+    window.version = '1.0.0';
 
     // Getting the latest version from GitHub
     const url = `https://api.github.com/repos/chinzistor/StreamerQnA/releases/latest`;
@@ -12,41 +11,43 @@ async function checkUpdate() {
             throw new Error(`HTTP error! Status: ${response.status}`);
         }
         const data = await response.json();
-        latestVersion = data.tag_name;
+        window.latestVersion = data.tag_name;
     } catch (error) {
         console.error('Error fetching the latest release:', error);
     }
 
     // Converting the versions to numbers
-    let versionSliced = version.replace('.', '0');
-    let latestVersionSliced = latestVersion.replace('.', '0');
+    let versionSliced = window.version.split('.');
+    let latestVersionSliced = window.latestVersion.split('.');
+    let versionValue = versionSliced[0] * 1000000 + versionSliced[1] * 1000 + versionSliced[2];
+    let latestVersionValue = latestVersionSliced[0] * 1000000 + latestVersionSliced[1] * 1000 + latestVersionSliced[2];
 
     // Checking if the current version is lower than the latest version or not
-    if (+versionSliced < +latestVersionSliced) {
+    if (+versionValue < +latestVersionValue) {
         // Showing update message to the user, if the latest version is higher than the current version
         const notification = document.getElementById('latestRelease');
         notification.style.fontSize = '10pt';
-        notification.style.fontWeight = window.dashboardStyle.buttons.unselected.italic;
-        notification.style.fontStyle = window.dashboardStyle.buttons.unselected.bold;
-        notification.style.color = window.dashboardStyle.buttons.unselected.color;
-        notification.style.fontFamily = window.dashboardStyle.buttons.unselected.font;
+        notification.style.fontWeight = localStorage.getItem(`dashboardButtonsunselectedItalic`);
+        notification.style.fontStyle = localStorage.getItem(`dashboardButtonsunselectedBold`);
+        notification.style.color = localStorage.getItem(`dashboardButtonsunselectedColor`);
+        notification.style.fontFamily = localStorage.getItem(`dashboardButtonsunselectedFont`);
         notification.style.textAlign = 'center';
         notification.style.lineHeight = 0;
         notification.style.display = '';
 
         // Providing a download link to the latest version
         const link = document.getElementById('updateLink');
-        link.textContent = latestVersion;
-        link.href =`https://github.com/chinzistor/StreamerQnA/releases/tag/${latestVersion}`;
+        link.textContent = window.latestVersion;
+        link.href =`https://github.com/chinzistor/StreamerQnA/releases/tag/${window.latestVersion}`;
 
         // Telling the user the current version
         const currentVersion = document.getElementById('currentVersion');
-        currentVersion.textContent = `Current version: ${version}`;
+        currentVersion.textContent = `Current version: ${window.version}`;
         currentVersion.style.fontSize = '10pt';
-        currentVersion.style.fontWeight = window.dashboardStyle.buttons.unselected.italic;
-        currentVersion.style.fontStyle = window.dashboardStyle.buttons.unselected.bold;
-        currentVersion.style.color = window.dashboardStyle.buttons.unselected.color;
-        currentVersion.style.fontFamily = window.dashboardStyle.buttons.unselected.font;
+        currentVersion.style.fontWeight = localStorage.getItem(`dashboardButtonsunselectedItalic`);
+        currentVersion.style.fontStyle = localStorage.getItem(`dashboardButtonsunselectedBold`);
+        currentVersion.style.color = localStorage.getItem(`dashboardButtonsunselectedColor`);
+        currentVersion.style.fontFamily = localStorage.getItem(`dashboardButtonsunselectedFont`);
         currentVersion.style.textAlign = 'center';
         currentVersion.style.lineHeight = 0;
         currentVersion.style.display = '';
