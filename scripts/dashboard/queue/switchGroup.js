46284e45f7cb3e18d22a75b45d5ab6b8f51b1916
chinzistor <illmate0226@gmail.com>
2024-09-14 15:20:56 +0200

    Script to switch groups on the Queue tab

---
+++ scripts/dashboard/queue/switchGroup.js
@@ -0,0 +1,21 @@
+// Switching groups in the dashboard
+function switchGroup(name) {
+    let groupNames = localStorage.getItem('commands').split('%-%');
+    for (let a = 0; a < groupNames.length; a++) {
+        // Removing spaces from group names for id purposes
+        let group = groupNames[a].replace(" ", "");
+        styleButton(group, 'unselected');
+        // Hiding all groups
+        document.getElementById(group).style.display = "none";
+    }
+    styleButton(name, 'selected');
+    // Showing the specific group
+    document.getElementById(name).style.display = "";
+
+    // Setting the clear group queue button's onclick function
+    document.getElementById('clearQueueConfigCell').onclick = function () {
+        clearGroup(name);
+    };
+    // Saving the last opened queue into localStorage
+    localStorage.setItem('openGroup', name);
+}
